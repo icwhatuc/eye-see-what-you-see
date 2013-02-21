f6cb382962715659973c57437bd6059f3c0c3cce
icwhatuc <eeicwhatuc@gmail.com>
2013-02-21 14:06:48 -0500

    eyegaze svm and haar (no previous info or optical flow yet)

--- eyegaze.cpp
+++ eyegaze.cpp
@@ -32,6 +32,9 @@ using namespace cv;
 using namespace std;
 
 void timing(bool start, string what="");
+Rect haarEyeClassify(gpu::CascadeClassifier_GPU &cascade, gpu::GpuMat &image);
+bool svmEyeClassify(CvSVM &svm, Mat &image);
+bool pointsOverlap(Point2f &p1, Point2f &p2);
 
 int main(int argc, const char *argv[])
 {
@@ -57,12 +60,10 @@ int main(int argc, const char *argv[])
 				diff_img, thresh_img,
 				hist(1,256,CV_32SC1);
 	Mat mat_prevframe(CAM_WIDTH, CAM_HEIGHT, CV_8UC3),
+		mat_prevframe_gray(CAM_WIDTH, CAM_HEIGHT, CV_8UC1),
 		mat_currframe(CAM_WIDTH, CAM_HEIGHT, CV_8UC3),
+		mat_currframe_gray(CAM_WIDTH, CAM_HEIGHT, CV_8UC1),
 		mat_diff, mat_thresh, mat_hist;
-
-	//DEBUG!!
-	Mat mat_prevframe_gray(CAM_WIDTH, CAM_HEIGHT, CV_8UC1),
-	    mat_currframe_gray(CAM_WIDTH, CAM_HEIGHT, CV_8UC1);
 
 	// SVM
 	CvSVM svm;
@@ -70,8 +71,14 @@ int main(int argc, const char *argv[])
 
 	// Haar
 	gpu::CascadeClassifier_GPU eye_cascade, eyepair_cascade;
-	eye_cascade.load(HAAR_EYE_FILE);
-	eyepair_cascade.load(HAAR_EYEPAIR_FILE);
+	if (!eye_cascade.load(HAAR_EYE_FILE)) {
+		fprintf(stderr, "ERROR: could not load haar cascade xml for eyes\n");
+		return -1;
+	}
+	if (!eyepair_cascade.load(HAAR_EYEPAIR_FILE)) {
+		fprintf(stderr, "ERROR: could not load haar cascade xml for eye pairs\n");
+		return -1;
+	}
 
 	// Blob detection parameters
 	SimpleBlobDetector::Params params;
@@ -93,7 +100,7 @@ int main(int argc, const char *argv[])
 	const float range[] = { 0, HIST_BINS } ;
 	const float* histRange = { range };
 	int bins = HIST_BINS;
-	int histCount, threshBin;
+	int hist_count, thresh_bin;
 
 	// Display windows
 	cvNamedWindow(W_COLOR, CV_WINDOW_NORMAL);
@@ -110,6 +117,7 @@ int main(int argc, const char *argv[])
 		capture >> mat_currframe;
 		currframe.upload(mat_currframe);
 		gpu::cvtColor(currframe, currframe_gray, CV_BGR2GRAY);
+		currframe_gray.download(mat_currframe_gray);
 		
 		// Obtain difference image and blur
 		gpu::absdiff(currframe_gray, prevframe_gray, diff_img);
@@ -118,22 +126,61 @@ int main(int argc, const char *argv[])
 		// Find threshold based on histogram
 		gpu::calcHist(diff_img,hist);
 		hist.download(mat_hist);
-		histCount = 0;
-		for (threshBin = HIST_BINS-1; threshBin > 0; threshBin--) {
-			histCount += mat_hist.at<uchar>(threshBin);
-			if (histCount > histThresholdPixels)
+		hist_count = 0;
+		for (thresh_bin = HIST_BINS-1; thresh_bin > 0; thresh_bin--) {
+			hist_count += mat_hist.at<uchar>(thresh_bin);
+			if (hist_count > histThresholdPixels)
 				break;
 		}
-		gpu::threshold(diff_img,thresh_img,threshBin,255,CV_THRESH_BINARY);
+		gpu::threshold(diff_img,thresh_img,thresh_bin,255,CV_THRESH_BINARY);
 
 		// Set blob thresholds, then detect blobs
-		params.minThreshold = threshBin-1;
-		params.maxThreshold = threshBin+1;
+		params.minThreshold = thresh_bin-1;
+		params.maxThreshold = thresh_bin+1;
 		Ptr<FeatureDetector> blob_detector = new SimpleBlobDetector(params);
 		blob_detector->create("SimpleBlob");
 		diff_img.download(mat_diff); // Blob detect doesn't work on GpuMat objects
 		blob_detector->detect(mat_diff, keypoints);
 		drawKeypoints(mat_currframe,keypoints,mat_currframe,CV_RGB(0,0,255));
+
+		// Iterate through keypoints and check using SVM and Haar cascades
+		for (int blob_num=0; blob_num<keypoints.size(); blob_num++) {
+			// Top left corner of square region
+			int x = keypoints[blob_num].pt.x - SVM_IMG_SIZE/2;
+			int y = keypoints[blob_num].pt.y - SVM_IMG_SIZE/2;
+			if (x < 0 || y < 0 || 
+				x + SVM_IMG_SIZE >= CAM_WIDTH ||
+				y + SVM_IMG_SIZE >= CAM_HEIGHT)
+				continue;
+
+			Rect candidate_region(x, y, SVM_IMG_SIZE, SVM_IMG_SIZE);
+			Mat mat_candidate_img = mat_currframe_gray(candidate_region);
+			gpu::GpuMat candidate_img(mat_candidate_img);
+
+			// SVM
+			bool svmDetected = svmEyeClassify(svm,mat_candidate_img);
+			if (svmDetected) {
+				// Draw circle for SVM
+				circle(
+					mat_currframe, 
+					Point(x+SVM_IMG_SIZE/2, y+SVM_IMG_SIZE/2), 
+					5, CV_RGB(255,0,0), 3
+				);
+			}
+
+			// Haar
+			Rect eye_rect = haarEyeClassify(eye_cascade,candidate_img);
+			bool haarDetected = eye_rect.area() > 1;
+			if (haarDetected) {
+				// Draw rectangle for Haar
+				rectangle(
+					mat_currframe,
+					Point(x+eye_rect.x, y+eye_rect.y),
+					Point(x+eye_rect.width+eye_rect.x, y+eye_rect.height+eye_rect.y),
+					CV_RGB(255,255,51), 2
+				);
+			}
+		}
 
 		// Show images
 		imshow(W_COLOR,mat_currframe);
@@ -152,7 +199,15 @@ int main(int argc, const char *argv[])
 	return 0;
 }
 
+bool pointsOverlap(Point2f &p1, Point2f &p2) {
+	int x = p1.x - SVM_IMG_HEIGHT/2;
+	int y = p1.y - SVM_IMG_HEIGHT/2;
+	return (p2.x < (x+SVM_IMG_HEIGHT) && p2.x > x
+		&& p2.y < (y+SVM_IMG_HEIGHT) && p2.y > y);
+}
+
 void timing(bool start, string what) {
+	return; //DEBUG
 	static struct timeval starttime, endtime;
 	static long mtime, seconds, useconds;
 	if (start) {
@@ -170,21 +225,40 @@ void timing(bool start, string what) {
 	}
 }
 
-/*
-Rect *haarEyeClassify(gpu::CascadeClassifier_GPU &cascade, gpu::GpuMat &image) {
-	gpu::GpuMat eyes, tmpImage;
-	const int scaleFactor = 1.2;
+Rect haarEyeClassify(gpu::CascadeClassifier_GPU &cascade, gpu::GpuMat &image) {
+	gpu::GpuMat eyes, eqImage;
+	const float scaleFactor = 1.2;
 	const int minNeighbors = 3;
 	const Size minSize = Size(20,20);
 
-	equalizeHist(image,tmpImage);
-	int numDetected = cascade.detectMultiScale(tmpImage, eyes, scaleFactor, minNeighbors, minSize);
+	gpu::equalizeHist(image, eqImage);
+	eqImage.convertTo(eqImage,CV_8U);
+	int numDetected = cascade.detectMultiScale(eqImage, eyes, scaleFactor, minNeighbors, minSize);
+
+	// If no eyes detected, return rectangle with area 1
 	if (numDetected == 0)
-		return 0;
+		return Rect(0,0,1,1);
 	
 	Mat tmpMat;
 	eyes.colRange(0, numDetected).download(tmpMat);
 	Rect *eyeRect = tmpMat.ptr<Rect>();
 	return eyeRect[0];
 }
-*/
+
+bool svmEyeClassify(CvSVM &svm, Mat &image) {
+	Mat eqImage;
+    
+	equalizeHist(image,eqImage);
+	Mat imageMat1D(1,eqImage.size().area(),CV_32FC1);
+	
+	int index = 0;
+	for (int r = 0; r<eqImage.rows; r++) {
+		for (int c = 0; c < eqImage.cols; c++) {
+			imageMat1D.at<float>(index++) = eqImage.at<uchar>(r,c);
+		}
+	}
+	
+	float retval = svm.predict(imageMat1D); // Non-eye: -1, Eye: +1
+	return retval>0;
+}
+
